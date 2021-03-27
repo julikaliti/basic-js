2ca05fd937aaeb9a77f316734da8ada1cba73657
julikaliti <yulikaliti@gmail.com>
2021-03-27 23:38:33 +0300

    init: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,14 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (Array.isArray(arr)) {
+    let transArray = [];
+    array.forEach(element => {
+      
+    });
+  } else {
+    throw new Error();
+  }
 };
