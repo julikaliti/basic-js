b6fff2c2c1333080572dd8e146fa618705401e8f
julikaliti <yulikaliti@gmail.com>
2021-03-28 23:37:29 +0300

    fix: handle a float number in the carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,18 +1,29 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-throw new CustomError('Not implemented');
+//throw new CustomError('Not implemented');
   // remove line with error and write your code here
-/*  const k = 0.693/HALF_LIFE_PERIOD;
-	let patt = /['0-9']/g;
-	let time = false;
-	if (patt.test(sampleActivity)) {
-		let n = parseInt(sampleActivity);
-		time = Math.ceil(Math.log(MODERN_ACTIVITY/parseInt(sampleActivity))/k);
-		
-	} else {time=false;}
-	return(time);*/
-};
+	if ((typeof sampleActivity === 'string')&&(typeof sampleActivity !== 'undefined')) {
+		const k = 0.693/HALF_LIFE_PERIOD;
+		let patt = /['0-9'\.]/g;
+		let time;
+		let check = sampleActivity.match(patt);
+		if ((check !== null)&&(check.length === sampleActivity.length) && (check.length > 0) && (parseFloat(check.join('')) > 0)) {
+			time = Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity))/k);
+			if (time > 0) {
+				return time;
+			} else {return false;}
+		} else {
+			console.log(false);
+			return false;
+		}
+
+	} else {
+		console.log(false);
+		return false;
+
+	} 
+}
