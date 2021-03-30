14c8624346f7ec847d5a4ccc71b5193aac4b240b
julikaliti <yulikaliti@gmail.com>
2021-03-30 21:42:29 +0300

    init: implement part of transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,11 +3,60 @@
 module.exports = function transform(arr) {
   //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+	let check = '';
   if (Array.isArray(arr)) {
-    let transArray = [];
-    array.forEach(element => {
-      
-    });
+    //if ((arr.includes('--discard-next'))||(arr.includes('--discard-prev'))||(arr.includes('--double-prev'))||(arr.includes('--double-next'))) {
+      transArr = arr.reduce((newArray, element, ind, arr) => {
+          //if ((typeof arr[ind-1] !== 'undefined')&&(typeof arr[ind+1] !== 'undefined')) {
+            newArray.push(element);
+
+            if (element === "--discard-prev") {
+              newArray.splice(newArray.length-1, 1);
+              if (ind > 0 && check !== 'deleted'){
+                newArray.splice(newArray.length-2, 1);
+              }
+            }
+    
+            if (arr[ind-1] === '--discard-next') {
+              newArray.splice(newArray.length-2, 1);
+              if (ind <= arr.length-1) {
+                newArray.splice(newArray.length-1, 1);
+                check = 'deleted';
+              } 
+            }
+            
+            if (element === '--discard-next' && ind === arr.length - 1) {
+              newArray.splice(newArray.length-1, 1);
+              
+            }
+            
+           if (element === '--double-prev') {
+              newArray.splice(newArray.length-1, 1);
+              if (ind > 0 && check !== 'deleted') {
+                newArray.splice(newArray.length-1, 0, arr[ind-1]);
+              }
+            } 
+              
+            if (element === '--double-next') {
+              newArray.splice(newArray.length-1, 1);
+              if (ind < arr.length-1)  {        
+                newArray.splice(ind, 0, arr[ind+1]);
+              }
+              
+            } 
+    
+            return newArray;
+          //}
+          
+          //console.log('Output: ', transArr);
+      }, []);
+      console.log(transArr);
+      return transArr;
+   /* } else {
+      console.log(arr);
+      return arr;
+    }*/
+
   } else {
     throw new Error();
   }
